94e1abca59e2831de1f0a9620b32360e3e9c4ee8
jalyice <jalyice@gmail.com>
2024-09-10 13:14:32 +0900

    변경

--- src/main/java/com/nhnacademy/thread/CounterThread.java
+++ src/main/java/com/nhnacademy/thread/CounterThread.java
@@ -18,16 +18,20 @@ import java.util.Objects;
 
 @Slf4j
 //TODO#1 CounterThread는 Thread를 상속 합니다.
-public class CounterThread {
+public class CounterThread extends Thread{
     private final long countMaxSize;
 
     private long count;
 
     public CounterThread(String name, long countMaxSize) {
         //TODO#2 name <-- null 이거나 공백 문자열이면 IllegalArgumentException이 발생 합니다.
-
+        if(name.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
         //TODO#3 countMaxSize <=0 이면 IllegalArgumentException이 발생 합니다.
-
+        if(countMaxSize <= 0) {
+            throw new IllegalArgumentException();
+        }
 
         this.setName(name);
         this.countMaxSize = countMaxSize;
@@ -44,9 +48,14 @@ public class CounterThread {
             - count : 1~10 까지 출력 됩니다.
             - thread name : this.getName()<- 호출, CounterThread 가 Thread를 extends 하고 있음.
          */
-
         do {
-
+            try {
+                Thread.sleep(1000);
+                count++;
+                log.debug(this.count + " " + this.getName());
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
         }while (count<countMaxSize);
     }
 }
