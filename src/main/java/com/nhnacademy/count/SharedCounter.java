e97264a020447ead0e66db91caf1bff21038006f
jalyice <jalyice@gmail.com>
2024-09-11 09:37:18 +0900

    변경

--- src/main/java/com/nhnacademy/count/SharedCounter.java
+++ src/main/java/com/nhnacademy/count/SharedCounter.java
@@ -28,33 +28,48 @@ public class SharedCounter {
         }
         this.count = count;
         //TODO#1-1 semaphore를 생성 합니다.( 동시에 하나의 Thread만 접근할 수 있습니다. ), permits prameter를 확인하세요.
-        semaphore = null;
+        semaphore = new Semaphore(1);
     }
 
-    public long getCount(){
+    public long getCount() throws InterruptedException {
         /*TODO#1-2 count 를 반환 합니다.
             semaphore.acquire()를 호출하여 허가를 획득 합니다.
             쓰레드가 작업이 완료되면
             semaphore.release()를 호출하여
             허가를 반환 합니다.
          */
-
-        return count;
+        semaphore.acquire();
+        try{
+            return count;
+        }finally{
+            semaphore.release();
+        }
     }
 
-    public long increaseAndGet(){
+    public long increaseAndGet() throws InterruptedException {
         /* TODO#1-3 count = count + 1 증가시키고 count를 반환 합니다.
            1-2 처럼 semaphore를 이용해서 동기화할 수 있도록 구현 합니다.
         */
-        count = count + 1;
-        return count;
+        semaphore.acquire();
+        try{
+            count = count + 1;
+            return count;
+        }finally {
+            semaphore.release();
+        }
+
     }
 
-    public long decreaseAndGet(){
+    public long decreaseAndGet() throws InterruptedException {
         /*TODO#1-4 count = count-1 감소시키고 count를 반환 합니다.
           1-2 처럼 semaphore를 이용해서 동기화할 수 있도록 구현 합니다.
         */
-        count = count - 1;
-        return count;
+        semaphore.acquire();
+        try{
+            count = count - 1;
+            return count;
+        }finally {
+            semaphore.release();
+        }
     }
 }
